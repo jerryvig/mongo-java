2e7190398a0226e94c56841ea569cb3d21aec903
Jerry Vigil <jerry.vigil@mktneutral.com>
2012-02-21 06:13:26 -0500

    Next commit for cboe weeklies.

--- rhino-js/NormalizeCBOEWeeklies.js
+++ rhino-js/NormalizeCBOEWeeklies.js
@@ -3,6 +3,7 @@ importPackage(com.mongodb);
 var m = new Mongo("localhost");
 var db = m.getDB("morningstar");
 var cboeWeekliesColl = db.getCollection("cboe_weeklies_eod");
+var cboeRevColl = db.getCollection("cboe_weeklies_revenues");
 var cboeWeekliesList = cboeWeekliesColl.distinct("ticker_symbol");
 
 db.getCollection("cboe_ticker_data").drop();
@@ -51,11 +52,51 @@ for ( var i=0; i<cboeWeekliesList.size(); i++ ) {
   }
   outDBObject.put("eod_data",eodList);
 
+  var sharpeRatio = 0.0;
   if ( sharpeCount > 0 ) {
      var avgChange = sumChange/sharpeCount;
      var stddevChange = Math.sqrt( sumChangeSquared/sharpeCount - avgChange*avgChange );
-     var sharpeRatio = avgChange/stddevChange;
+     sharpeRatio = avgChange/stddevChange;
      outDBObject.put("sharpe_ratio",sharpeRatio); 
+  }
+
+  sorter = new BasicDBObject();
+  sorter.put("period",1);
+  var revList = new BasicDBList();
+
+  var cur2 = cboeRevColl.find(query).sort(sorter);
+  var revCount = 0;
+  var prevRev = 0.0;
+  var sumRevGrowth = 0.0;
+  var sumRevGrowthSquared = 0.0;
+
+  while ( cur2.hasNext() ) {
+     var row = cur2.next();
+     var revObj = new BasicDBObject();
+     revObj.put("period",row.get("period"));
+     revObj.put("revenue",row.get("revenue"));
+     revList.put(revCount,revObj);
+     if ( revCount > 1 ) {
+	var revGrowth = (row.get("revenue")-prevRev)/prevRev;
+        sumRevGrowth += revGrowth;
+        sumRevGrowthSquared += revGrowth*revGrowth;
+     }
+     prevRev = row.get("revenue");
+     revCount++;   
+  }
+  outDBObject.put("revenue_data",revList);
+
+  var sharpeRevGrowth = 0.0;
+  if ( revCount > 0 ) {
+     var avgRevGrowth = sumRevGrowth/revCount;
+     var stddevRevGrowth = Math.sqrt( sumRevGrowthSquared - avgRevGrowth*avgRevGrowth );
+     var sharpeRevGrowth = avgRevGrowth/stddevRevGrowth;
+     outDBObject.put("avg_rev_growth",avgRevGrowth);
+     outDBObject.put("sharpe_rev_growth",sharpeRevGrowth);
+  }
+
+  if ( sharpeRatio > 0.0 || sharpeRevGrowth > 0.0 ) {
+    outDBObject.put("hypotenuse",Math.sqrt( 9.0*sharpeRatio*sharpeRatio + sharpeRevGrowth*sharpeRevGrowth ));
   }
 
   cboeTickerColl.insert( outDBObject ); 
